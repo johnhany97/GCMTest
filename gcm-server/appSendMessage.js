00eb24370e3e7b71f0d891fd275e41cdf45a1f84
John Hany <johnhany97@gmail.com>
2014-07-28 17:52:38 +0200

    Update appSendMessage.js

--- gcm-server/appSendMessage.js
+++ gcm-server/appSendMessage.js
@@ -1,6 +1,6 @@
 var gcm = require('node-gcm');
 
-var sender = new gcm.Sender('AIzaSyC8LGUhFTSUN38CNg0nqSXPrcKbSbMSvEQ');
+var sender = new gcm.Sender('AIzaSyDGd6hLXVDkzu3DLXiATM-SbCmNJ3Up2kQ');
  
 var message = new gcm.Message();
 
